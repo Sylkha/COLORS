7e4359353cb43f9b45f1895b6167282d58a67f2b
Silvia Osoro <37350241+Sylkha@users.noreply.github.com>
2019-06-27 16:52:57 +0200

    Add files via upload

---
+++ RippleEffectShader/RipplePostProcessor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RipplePostProcessor : MonoBehaviour
+{
+    public Material RippleMaterial;
+    public float MaxAmount = 50f;
+
+    [Range(0, 1)]
+    public float Friction = .9f;
+
+    private float Amount = 0f;
+
+
+    public static Animator anim;
+
+    //public static bool ripple_bool = false;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            this.Amount = this.MaxAmount;
+            Vector3 pos = Input.mousePosition;
+            this.RippleMaterial.SetFloat("_CenterX", pos.x);
+            this.RippleMaterial.SetFloat("_CenterY", pos.y);
+            //ripple_bool = false;
+        }
+        
+
+        this.RippleMaterial.SetFloat("_Amount", this.Amount);
+        this.Amount *= this.Friction;
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, this.RippleMaterial);
+    }
+
+    //public void CambioEscena()
+    //{
+    //    Scene sc = SceneManager.GetActiveScene();
+
+    //    if(sc.name != "SimonDice")
+    //        SceneManager.LoadScene(1);
+    //    else if(sc.name == "SimonDice")
+    //        SceneManager.LoadScene(6);
+    //}
+}
